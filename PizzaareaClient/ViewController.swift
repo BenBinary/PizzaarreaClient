3b56e0b06d1748c94c19f967de6e7dda1246c3ed
Bene <bk191414@gmail.com>
2019-03-26 12:12:06 +0100

    Initial Commit

---
+++ PizzaareaClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PizzaareaClient
+//
+//  Created by Benedikt Kurz on 26.03.19.
+//  Copyright © 2019 Benedikt Kurz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
